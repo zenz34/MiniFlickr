de58a8d5c733a568d5b54c58cd2edfe57fbe2ffd
ZhenkangZhao <fyfyzzk@hotmail.com>
2017-12-02 17:25:07 -0800

    first commit server basic function, return index.html

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+
+var flickrServer = function() {
+    var restRouter = require('./routes/rest');      //  ?
+    app.use('/', express.static(__dirname + '/'));  //  ?
+    app.use('/api/v1', restRouter);                 //  ?
+    app.listen(3000);
+    console.log('Server initialized!');
+    
+};
+
+flickrServer();
+
+
+/**
+express()
+app.use()  
+    route name and its handler? 
+    what about the 1st '/' , 
+        express_static()?
+
+app.listen(port);
+    start the server and  listen to the port
+
+
+
+    express_static()
+        Static files are files that clients download as they are from the server. Create a new directory, public. Express, by default does not allow you to serve static files. You need to enable it using the following built-in middleware.
+
+        primitevie idea
+        It means that it enable the server to use the files in the '\' dir.
+
+ 
+ 
+ 
+ How to start index.html?
+
+
+ rest.js
+ x  res.send('../index.html');
+ may be index.html is a default main page.
+
+
+        * 
+ */
